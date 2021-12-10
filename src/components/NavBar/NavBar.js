7e4d52e75c0deb4e04fc57eca91adfa45bbe986d
RotemLibrati <rotemlibrati1@gmail.com>
2021-12-10 15:15:57 +0200

    Added new route for favorites page

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
+import { HashRouter as Router, Link } from "react-router-dom";
 
 const NavBar = () => {
   const [value, setValue] = useState(0);
@@ -11,18 +12,18 @@ const NavBar = () => {
   };
 
   return (
-    <AppBar position="static" color="transparent" style={{ position: "fixed", top: 0 }}>
-      <Tabs
-        value={value}
-        onChange={handleChange}
-        aria-label="Navigation"
-        indicatorColor="primary"
-        textColor="primary"
-      >
-        <Tab label="Home" index={0} />
-        <Tab label="Favorites" index={1} />
-      </Tabs>
-    </AppBar>
+      <AppBar position="static" color="transparent" style={{ position: "fixed", top: 0 }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="Navigation"
+          indicatorColor="primary"
+          textColor="primary"
+        >
+          <Tab label="Home" index={0} component={Link} to="/"/>
+          <Tab label="Favorites" index={1} component={Link} to="/fav" />
+        </Tabs>
+      </AppBar>
   );
 };
 
