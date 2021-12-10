7bb34d4670a1cc5dc99c9ad29b83f6052156abe5
RotemLibrati <rotemlibrati1@gmail.com>
2021-12-10 15:14:04 +0200

    Added new route to favorites page

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
-import { Home } from "pages";
+import React, { useState } from "react";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Home, Favorites } from "pages";
 import { ThemeProvider } from "theme";
 import NavBar from "components/NavBar";
 
@@ -8,9 +8,10 @@ const AppRouter = () => {
   return (
     <ThemeProvider>
       <Router>
-        <NavBar />
+        <NavBar/>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route path="/fav" component={Favorites} />
         </Switch>
       </Router>
     </ThemeProvider>
