ee8356d334c8d340ffaa77c14ee4f1d5ceda1e76
RotemLibrati <rotemlibrati1@gmail.com>
2021-12-10 15:26:29 +0200

    Task 2 Adding all the requirements in task 2.

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -9,7 +9,12 @@ import * as S from "./style";
 const UserList = ({ users, isLoading }) => {
   const [hoveredUserId, setHoveredUserId] = useState();
   const [markedCountry, setMarkedCountry] = useState([]);
+  const [favorites, setFavorites] = useState([]);
   const [filter, setFilter] = useState(false);
+  useEffect(() => {
+    if (JSON.parse(localStorage.getItem('favorites')))
+      setFavorites(JSON.parse(localStorage.getItem('favorites')));
+  }, []);
   const handleChanged = (value, checked, label) => {
     if (checked) {
       markedCountry.push(...[label]);
@@ -22,12 +27,26 @@ const UserList = ({ users, isLoading }) => {
   const handleMouseEnter = (index) => {
     setHoveredUserId(index);
   };
+  const handleMouseClicked = async (index, user) => {
+    const localData = localStorage.getItem('favorites');
+    if (localData) {
+      if (!localData.includes(JSON.stringify(user))) {
+        favorites.push(...[user]);
+        localStorage.setItem('favorites', JSON.stringify(favorites));
+      } else {
+        setFavorites(favorites.filter(e => JSON.stringify(e) !== JSON.stringify(user)));
+        localStorage.setItem('favorites', JSON.stringify(favorites.filter(e => JSON.stringify(e) !== JSON.stringify(user))));
+      }
+    } else {
+      favorites.push(...[user]);
+      if (favorites)
+        localStorage.setItem('favorites', JSON.stringify(favorites));
+    }
+  };
 
   const handleMouseLeave = () => {
     setHoveredUserId();
   };
-  console.log(users);
-
 
   return (
     <S.UserList>
@@ -37,7 +56,7 @@ const UserList = ({ users, isLoading }) => {
         <CheckBox value="CA" label="Canada" onChange={handleChanged} />
         <CheckBox value="GE" label="Germany" onChange={handleChanged} />
         {/* Bonus */}
-        <CheckBox value="DE" label="Denmark" onChange={handleChanged} /> 
+        <CheckBox value="DE" label="Denmark" onChange={handleChanged} />
         {/* Bonus */}
       </S.Filters>
       <S.List>
@@ -61,55 +80,69 @@ const UserList = ({ users, isLoading }) => {
                   {user?.location.city} {user?.location.country}
                 </Text>
               </S.UserInfo>
-              <S.IconButtonWrapper isVisible={index === hoveredUserId}>
-                <IconButton>
-                  <FavoriteIcon color="error" />
-                </IconButton>
-              </S.IconButtonWrapper>
-            </S.User>
-          );
-        }) 
-        : (
-          
-          users.map((user, index) => {
-            return (
-              markedCountry.includes(user.location.country) &&  
-              <S.User
-                key={index}
-                onMouseEnter={() => handleMouseEnter(index)}
-                onMouseLeave={handleMouseLeave}
-              >
-                <S.UserPicture src={user?.picture.large} alt="" />
-                <S.UserInfo>
-                  <Text size="22px" bold>
-                    {user?.name.title} {user?.name.first} {user?.name.last}
-                  </Text>
-                  <Text size="14px">{user?.email}</Text>
-                  <Text size="14px">
-                    {user?.location.street.number} {user?.location.street.name}
-                  </Text>
-                  <Text size="14px">
-                    {user?.location.city} {user?.location.country}
-                  </Text>
-                </S.UserInfo>
-                <S.IconButtonWrapper isVisible={index === hoveredUserId}>
-                  <IconButton>
+              {JSON.stringify(favorites).includes(JSON.stringify(user)) ? (
+                <S.IconButtonWrapper isVisible={true} >
+                  <IconButton onClick={() => handleMouseClicked(index, user)} >
                     <FavoriteIcon color="error" />
                   </IconButton>
-                </S.IconButtonWrapper>
-              </S.User>
-            );
-          })
-        )}
+                </S.IconButtonWrapper>)
+                :
+                (<S.IconButtonWrapper isVisible={index === hoveredUserId}>
+                  <IconButton onClick={() => handleMouseClicked(index, user)} >
+                    <FavoriteIcon color="error" />
+                  </IconButton>
+                </S.IconButtonWrapper>)}
+            </S.User>
+          );
+        })
+          : (
+
+            users.map((user, index) => {
+              return (
+                markedCountry.includes(user.location.country) &&
+                <S.User
+                  key={index}
+                  onMouseEnter={() => handleMouseEnter(index)}
+                  onMouseLeave={handleMouseLeave}
+                >
+                  <S.UserPicture src={user?.picture.large} alt="" />
+                  <S.UserInfo>
+                    <Text size="22px" bold>
+                      {user?.name.title} {user?.name.first} {user?.name.last}
+                    </Text>
+                    <Text size="14px">{user?.email}</Text>
+                    <Text size="14px">
+                      {user?.location.street.number} {user?.location.street.name}
+                    </Text>
+                    <Text size="14px">
+                      {user?.location.city} {user?.location.country}
+                    </Text>
+                  </S.UserInfo>
+                  {JSON.stringify(favorites).includes(JSON.stringify(user)) ? (
+                    <S.IconButtonWrapper isVisible={true} >
+                      <IconButton onClick={() => handleMouseClicked(index, user)} >
+                        <FavoriteIcon color="error" />
+                      </IconButton>
+                    </S.IconButtonWrapper>)
+                    :
+                    (<S.IconButtonWrapper isVisible={index === hoveredUserId}>
+                      <IconButton onClick={() => handleMouseClicked(index, user)} >
+                        <FavoriteIcon color="error" />
+                      </IconButton>
+                    </S.IconButtonWrapper>)}
+                </S.User>
+              );
+            })
+          )}
 
 
-        
+
         {isLoading && (
           <S.SpinnerWrapper>
             <Spinner color="primary" size="45px" thickness={6} variant="indeterminate" />
           </S.SpinnerWrapper>
         )}
-      </S.List> 
+      </S.List>
     </S.UserList>
   );
 };
